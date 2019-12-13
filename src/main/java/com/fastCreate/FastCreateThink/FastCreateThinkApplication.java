cc545d6b58a7550584353669383af0ffac8bfbc6
yao295904105 <15044715085@163.com>
2019-12-13 17:48:01 +0800

    基本提交

---
+++ src/main/java/com/fastCreate/FastCreateThink/FastCreateThinkApplication.java
@@ -0,0 +1,13 @@
+package com.fastCreate.FastCreateThink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCreateThinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastCreateThinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastCreate/FastCreateThink/FastCreateThinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastCreate.FastCreateThink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCreateThinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
